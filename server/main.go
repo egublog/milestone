de7239cff8e2547177aea47b2b017b5e5117a2c4
egublog <egukei0729@gmail.com>
2023-07-03 17:40:09 +0900

    modify: APIの取得がエラーになるのでCORS設定を追加

--- server/main.go
+++ server/main.go
@@ -5,12 +5,21 @@ import (
 )
 
 type Milestone struct {
-	ID    int 	 `json:"id"`
+	ID    int    `json:"id"`
 	Title string `json:"title"`
 }
 
 func main() {
 	r := gin.Default()
+
+	// CORS設定
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Reactアプリのオリジンを許可
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")                  // 許可するHTTPメソッドを設定
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")         // 許可するヘッダーを設定
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")                      // プリフライトリクエストのキャッシュ時間を設定
+		c.Next()
+	})
 
 	r.GET("/api/milestones", func(c *gin.Context) {
 		milestone := []Milestone{
