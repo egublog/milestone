beee4b8d2baff2a934fe28b3a44da300347bb6a8
egublog <egukei0729@gmail.com>
2023-07-03 20:28:36 +0900

    refactor: ダミーデータをdb/db.goから取得

--- server/handlers/milestone_list.go
+++ server/handlers/milestone_list.go
@@ -1,16 +1,12 @@
 package handlers
 
-import	(
+import (
+	"github.com/egublog/milestone/server/db"
 	"github.com/gin-gonic/gin"
-	"github.com/egublog/milestone/server/models"
 )
 
 func GetMilestones(c *gin.Context) {
-	milestones := []models.Milestone{
-		{ID: 1, Title: "Milestone 1"},
-		{ID: 2, Title: "Milestone 2"},
-		{ID: 3, Title: "Milestone 3"},
-	}
+	milestones := db.GetDummyMilestones()
 
 	c.JSON(200, milestones)
 }
